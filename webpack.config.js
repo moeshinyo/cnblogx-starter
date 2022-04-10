86aaea539d19ce230a49fe72952ef44bd10ad75b
moeshinyo <moeshinyo@qq.com>
2022-04-10 11:37:46 +0800

    improve ts support

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { HtmlWebpackSkipAssetsPlugin } = require('html-webpack-skip-assets-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const webpack = require('webpack');
@@ -52,16 +53,22 @@ module.exports = (env, argv) => {
         module: {
             rules: [
                 {
-                    test: /\.(m?js|ts)$/i,
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'babel-loader', 
+                        {
+                            loader: 'ts-loader', 
+                            options: {
+                                transpileOnly: true
+                              }
+                        },
+                    ], 
+                }, 
+                {
+                    test: /\.(m?js)$/i,
                     exclude: /node_modules/i,
-                    use: [{
-                        loader: 'babel-loader', options: {
-                            presets: [
-                                ['@babel/preset-env'], 
-                                ['@babel/preset-typescript']
-                            ]
-                        }
-                    }],
+                    use: ['babel-loader'],
                 },
                 {
                     test: /\.s[ac]ss$/i,
@@ -95,6 +102,7 @@ module.exports = (env, argv) => {
             extensions: ['.ts', '.js'],
         },
         plugins: [
+            new ForkTsCheckerWebpackPlugin(), 
             new ESLintPlugin({
                 extensions: ['ts', 'js'], 
             }), 
